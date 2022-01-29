17b17aea1e7d966180eb1b0f0a1fffa7d16aab74
sharp_viper <andrrrew@ya.ru>
2022-01-29 13:56:25 +0700

    Forget to attach some files

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegUser, LogMeIn, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['my_secret_key']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 

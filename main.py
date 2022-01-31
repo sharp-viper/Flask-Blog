7b36921e60aace4eb4644527a1f16fa11529eaf8
sharp_viper <andrrrew@ya.ru>
2022-01-31 15:08:53 +0700

    create db?!

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ class Comment(db.Model):
     parent_post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
     parent_post = relationship('BlogPost', back_populates='comments')
 
-# db.create_all()
+db.create_all()
 
 
 def admin_only(f):

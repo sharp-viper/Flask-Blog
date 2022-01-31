940c3e7499ee2c87a3cea937fddb315e6995f44e
sharp_viper <andrrrew@ya.ru>
2022-01-31 15:00:28 +0700

    SQLite to Postgres

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegUser, LogMeIn, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 
 app = Flask(__name__)
@@ -19,7 +20,8 @@ Bootstrap(app)
 
 
 ## CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
